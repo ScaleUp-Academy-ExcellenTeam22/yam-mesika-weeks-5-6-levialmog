c17d7e5a4e06a7cfd23976d728ff7385f0781b46
Almog <almoglevi1008@gmail.com>
2022-03-19 23:00:54 +0200

    Updateed file

--- piece_of_cake.py
+++ piece_of_cake.py
@@ -1,15 +1,17 @@
 def get_recipe_price(prices=None, optionals=None, **ingredients):
-    if optionals is None:
-        optionals = []
-    if prices is None:
-        prices = {}
 
-    shopping_cart = 0
-    for price in prices:
-        if price not in optionals:
-            num_for_mul = ingredients[price]/100
-            shopping_cart = shopping_cart + int(prices[price])*num_for_mul
-    print(int(shopping_cart))
+    if prices is not None and ingredients is not None and len(ingredients) == len(prices):
+        if optionals is None:
+            optionals = []
+
+        shopping_cart = 0
+        for price in prices:
+            if price not in optionals:
+                num_for_mul = ingredients[price]/100
+                shopping_cart = shopping_cart + int(prices[price])*num_for_mul
+        print(int(shopping_cart))
+    else:
+        print("Bad input")
 
 
-get_recipe_price({})
+get_recipe_price({'chocolate': 18, 'milk': 8}, chocolate=200, milk=100)
