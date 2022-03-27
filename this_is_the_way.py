9934aeb3958aa2a0482eba0db08e1c436038e92c
Almog <almoglevi1008@gmail.com>
2022-03-27 14:02:42 +0300

    Fixed the code according to comments in pull request.

--- this_is_the_way.py
+++ this_is_the_way.py
@@ -1,16 +1,15 @@
 import os
 
 
-def this_is_the_way():
-    res_lst = []
-    path = input("Enter path to directory: ")
+def this_is_the_way(directory_path):
+    files_that_start_with_deep = []
 
-    for files in os.listdir(path):
-        if files.startswith('deep'):
-            res_lst.append(files)
+    for files in os.listdir(directory_path):
+        if files.startswith("deep"):
+            files_that_start_with_deep.append(files)
 
-    return res_lst
+    return files_that_start_with_deep
 
 
-this_is_the_way()
-
+if __name__ == "__main__":
+    this_is_the_way("C:\\yam_mesika_ex\\Notebooks-master\\Notebooks-master\\week05\\images")
