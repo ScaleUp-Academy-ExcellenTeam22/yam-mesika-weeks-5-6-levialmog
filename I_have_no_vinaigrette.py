36d48ff7b63effb47f92065888ef9afcd53a52b6
Almog <almoglevi1008@gmail.com>
2022-03-27 14:27:08 +0300

    Fixed the code according to comments in pull request.

--- I_have_no_vinaigrette.py
+++ I_have_no_vinaigrette.py
@@ -3,26 +3,21 @@ import random
 import re
 
 
-def days_between(d1, d2):
-    d1 = datetime.datetime.strptime(d1, "%Y-%m-%d")
-    d2 = datetime.datetime.strptime(d2, "%Y-%m-%d")
+def days_between(start_date, end_date):
+    d1 = datetime.datetime.strptime(start_date, "%Y-%m-%d")
+    d2 = datetime.datetime.strptime(end_date, "%Y-%m-%d")
     return abs(d1 - d2)
 
 
-def i_have_no_vinaigrette():
-    date1, date2 = input("Enter two dates in format YYYY-MM-DD and date1 smaller than date2: ").split()
-    if re.match('[0-9]{4}-[0-9]{2}-[0-9]{2}', date1) and re.match('[0-9]{4}-[0-9]{2}-[0-9]{2}', date2) and date1 < date2:
-        time_between_dates = days_between(date1, date2)
-        days_between_dates = time_between_dates.days
-        random_number_of_days = random.randrange(days_between_dates)
-        random_date = datetime.datetime.strptime(date1, "%Y-%m-%d") + datetime.timedelta(days=random_number_of_days)
-        print(random_date.date())
-
-    else:
-        print("Invalid input")
+def i_have_no_vinaigrette(start_date, end_date):
+    time_between_dates = days_between(start_date, end_date)
+    random_number_of_days = random.randrange(time_between_dates.days)
+    random_date = datetime.datetime.strptime(start_date, "%Y-%m-%d") + datetime.timedelta(days=random_number_of_days)
+    print(random_date.date())
 
     if random_date.weekday() == 0:
         print("I have no vinaigrette!")
 
 
-i_have_no_vinaigrette()
+if __name__ == "__main__":
+    i_have_no_vinaigrette("1997-10-10", "2000-10-10")
